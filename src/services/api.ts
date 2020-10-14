3a0c1100826c0b7f7d750884467ac025bad32bd0
Andre Dal Santos <andredalsantos@gmail.com>
2020-10-14 11:27:24 -0300

    challenge ok

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
+  // baseURL: 'http://10.0.2.2:3333', // uso com dispositivo físico android
+  // baseURL: 'http://192.168.0.106:3333', // uso com dispositivo físico android
 });
 
 export default api;
